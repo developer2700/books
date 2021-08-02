9afa5cab611aa963a2406fb08fbcf6ad1093b5ba
Mohammad Ghamari <Mohammad2700@gmail.com>
2021-08-02 05:59:25 +0430

    add route api file

--- routes/api.php
+++ routes/api.php
@@ -4,4 +4,9 @@ use Illuminate\Http\Request;
 
 Route::group(['namespace' => 'Api'], function () {
 
+    Route::get('books/export', 'BooksController@export');
+    Route::apiResource('books', 'BooksController');
+    Route::apiResource('authors', 'AuthorsController');
+    Route::post('upload-file', 'UploadController@upload');
+    Route::get('uploads/{filename}', 'UploadController@download');
 });
